acc9dd6105b6e9aa60568ab931d7c5d3a9c7f2bc
Raelhoff <raelhoff@gmail.com>
2016-02-20 14:29:02 -0200

    4

--- js/angular/appformulario.controller.js
+++ js/angular/appformulario.controller.js
@@ -1,6 +1,13 @@
 app.controller('appformulario',function($scope){
     $scope.estados = ['RS','RO','RR','SC','SP','SE','RJ'];
     $scope.pessoa = novaPessoa();
+    
+    $scope.pessoas = [];
+    $scope.salvarPessoa = function(pessoa){
+        $scope.pessoas.push(pessoa);
+        $scope.pessoas = novaPessoa();
+    }
+    
     console.log($scope.pessoa);
 });
 
